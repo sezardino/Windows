beecf82021fab77ab1f498d517aeb63f5b1d15a1
Edward <e.arekhva@gmail.com>
2020-09-06 11:59:08 +0200

    add gh-pages

--- js/modules/utils.js
+++
@@ -1,9 +0,0 @@
-export const chectInputForNumbers = (selector) => {
-  const inputs = document.querySelectorAll(selector);
-
-  inputs.forEach((input) => {
-    input.addEventListener('input', () => {
-      input.value = input.value.replace(/\D/, '');
-    })
-  });
-};
